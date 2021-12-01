e8ea33c44e20ff5357206facc11cce2b3f1d55be
SucioLoco <irelandrj@yahoo.com>
2021-12-01 11:03:48 -0500

    added second component to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
+import ColorChangeButton from './ColorChangeButton';
 
 function App() {
   const [color, setColor] = useState('')
@@ -7,6 +8,9 @@ function App() {
     <div className= {`react-root ${color}`}>
       <div className='centered'>
         <h1>Color Picker</h1>
+        <ColorChangeButton />
+        <ColorChangeButton />
+        <ColorChangeButton />
         <button className='red' onClick={() => setColor('red')}>red</button>
         <button className='blue' onClick={() => setColor('blue')}>blue</button>
         <button className='yellow' onClick={() => setColor('yellow')}>yellow</button>
---
+++ src/ColorChangeButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ColorChangeButton () {
+    return (
+        <button>Hi!</button>
+    )
+}
+
+export default ColorChangeButton
