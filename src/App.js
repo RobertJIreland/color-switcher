346a65ed3b8281fc7b59a5060b764bd1a498a122
SucioLoco <irelandrj@yahoo.com>
2021-12-01 10:54:06 -0500

    buttons change color of screen

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import './App.css';
 function App() {
   const [color, setColor] = useState('')
   return (
-    <div className='react-root'>
+    <div className= {`react-root ${color}`}>
       <div className='centered'>
         <h1>Color Picker</h1>
-        {color}
         <button className='red' onClick={() => setColor('red')}>red</button>
         <button className='blue' onClick={() => setColor('blue')}>blue</button>
         <button className='yellow' onClick={() => setColor('yellow')}>yellow</button>
